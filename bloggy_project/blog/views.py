5296806fbd12535d033a8395927f8c5e49f98817
Copser <iamrmot@gmail.com>
2015-09-21 22:29:29 +0200

    Making are templates directory, working whit new views.py

--- bloggy_project/blog/views.py
+++ bloggy_project/blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Context, loader
 
+from blog.models import Post
 # Create your views here.
+
+
+def index(request):
+    """TODO: Docstring for index.
+    :returns: TODO
+
+    """
+    latest_posts = Post.objects.all().order_by('-created_at')
+    t = loader.get_template('blog/index.html')
+    c = Context({'latest_posts': latest_posts, })
+    return HttpResponse(t.render(c))
--- bloggy_project/bloggy_project/settings.py
+++ bloggy_project/bloggy_project/settings.py
@@ -15,7 +15,6 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
@@ -102,3 +101,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DIRS = [os.path.join(BASE_DIR, 'templates')]
