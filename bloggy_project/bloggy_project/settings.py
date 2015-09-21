59fa199ae364b6aeff09cde997f00837e1d49795
Copser <iamrmot@gmail.com>
2015-09-21 22:49:57 +0200

    Revriting Templates Dirs path

--- bloggy_project/bloggy_project/settings.py
+++ bloggy_project/bloggy_project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'bloggy_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -99,7 +99,4 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
 STATIC_URL = '/static/'
-
-DIRS = [os.path.join(BASE_DIR, 'templates')]
