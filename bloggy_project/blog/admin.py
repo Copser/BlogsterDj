eba6a34e97d7c03580577bc5e29bef514946200d
Copser <iamrmot@gmail.com>
2015-09-21 22:15:49 +0200

    Activatin django admin

--- bloggy_project/blog/admin.py
+++ bloggy_project/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from blog.models import Post
+
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+
+    """Docstring for PostAdmin. """
+    list_dispaly = ('title', 'created_at')
+
+
+admin.site.register(Post, PostAdmin)
