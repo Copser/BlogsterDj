27be1472511dde1b5ea29fbcf09f8b5135f3199e
Copser <iamrmot@gmail.com>
2015-09-21 22:04:48 +0200

    Writing test for blog/models.py

--- bloggy_project/blog/tests.py
+++ bloggy_project/blog/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from models import Post
+
 
 # Create your tests here.
+class PostTests(TestCase):
+
+    """Docstring for PostTests. """
+    def test_str(self):
+        """TODO: Docstring for test_str.
+        :returns: TODO
+
+        """
+        my_title = Post(title="This is a basic title for a basic test case")
+        self.assertEquals(
+            str(my_title), 'This is a basic title for a basic test case',
+        )
