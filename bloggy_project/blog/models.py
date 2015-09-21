e1465dcda94f9420922bc2886b1b584889290055
Copser <iamrmot@gmail.com>
2015-09-21 21:14:23 +0200

    Definig are first model

--- bloggy_project/blog/models.py
+++ bloggy_project/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+
+    """Docstring for Post. """
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
