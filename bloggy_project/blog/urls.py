dd57d34a4eb30bdd2b22c7a4ba75ef877c21516a
Copser <iamrmot@gmail.com>
2015-09-22 06:22:24 +0200

    Rebuilding are url so post url would display nicer.

--- bloggy_project/blog/urls.py
+++ bloggy_project/blog/urls.py
@@ -3,5 +3,5 @@ from blog import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<post_id>\d+)/$', views.post, name='post'),
+    url(r'^(?P<post_url>\w+)/$', views.post, name='post'),
 ]
--- bloggy_project/blog/views.py
+++ bloggy_project/blog/views.py
@@ -13,16 +13,20 @@ def index(request):
     """
     latest_posts = Post.objects.all().order_by('-created_at')
     t = loader.get_template('blog/index.html')
-    c = Context({'latest_posts': latest_posts, })
+    context_dict = {'latest_posts': latest_posts, }
+    for post in latest_posts:
+        post.url = post.title.replace(' ', '_')
+    c = Context(context_dict)
     return HttpResponse(t.render(c))
 
 
-def post(request, post_id):
+def post(request, post_url):
     """TODO: Docstring for post.
     :returns: TODO
 
     """
-    single_post = get_object_or_404(Post, pk=post_id)
+    single_post = get_object_or_404(Post,
+                                    title=post_url.replace('_', ' '))
     t = loader.get_template('blog/post.html')
     c = Context({'single_post': single_post, })
     return HttpResponse(t.render(c))
