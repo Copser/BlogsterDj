418b88df01da203b96f1eff8f1c1cf8a4b3531dd
Copser <iamrmot@gmail.com>
2015-09-22 05:49:57 +0200

    Making a new views for are blog context

--- bloggy_project/blog/views.py
+++ bloggy_project/blog/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.template import Context, loader
+from django.shortcuts import get_object_or_404
 
 from blog.models import Post
 # Create your views here.
@@ -13,4 +14,15 @@ def index(request):
     latest_posts = Post.objects.all().order_by('-created_at')
     t = loader.get_template('blog/index.html')
     c = Context({'latest_posts': latest_posts, })
+    return HttpResponse(t.render(c))
+
+
+def post(request, post_id):
+    """TODO: Docstring for post.
+    :returns: TODO
+
+    """
+    single_post = get_object_or_404(Post, pk=post_id)
+    t = loader.get_template('blog/post.html')
+    c = Context({'single_post': single_post, })
     return HttpResponse(t.render(c))
