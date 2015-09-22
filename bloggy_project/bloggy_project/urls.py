5c8a8c06494682efca261194a86bdf2427be1a18
Copser <iamrmot@gmail.com>
2015-09-22 07:10:25 +0200

    Updating project level url and post.html so we can propely display and load pictures from media_root

--- bloggy_project/bloggy_project/urls.py
+++ bloggy_project/bloggy_project/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls')),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': MEDIA_ROOT}),
 ]
