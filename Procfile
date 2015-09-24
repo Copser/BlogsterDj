02ad470a9aeb20607835c83bc92ccb3426b4e67c
Copser <iamrmot@gmail.com>
2015-09-24 15:44:49 +0200

    Resolving Heroku Deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn bloggy_project.wsgi --log-file -
+web: gunicorn bloggy_project.wsgi --log-file - 
--- bloggy_project/bloggy_project/wsgi.py
+++ bloggy_project/bloggy_project/wsgi.py
@@ -7,11 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bloggy_project.settings")
 
 application = Cling(get_wsgi_application())
