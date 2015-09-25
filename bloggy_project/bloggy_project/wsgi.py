904ad833ff1142c8cf8b17145f8e7d236e768b85
Copser <iamrmot@gmail.com>
2015-09-25 05:29:43 +0200

    Preparing app for Heroku deployment

--- bloggy_project/bloggy_project/wsgi.py
+++ bloggy_project/bloggy_project/wsgi.py
@@ -7,8 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
+import os
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bloggy_project.settings")
+
+application = get_wsgi_application()
