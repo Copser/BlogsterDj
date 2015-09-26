14fe1581ae41b63f3726ee896f194932d61e2a43
Copser <iamrmot@gmail.com>
2015-09-26 14:21:25 +0200

    Reseolving Heroku error

--- bloggy_project/bloggy_project/settings.py
+++ bloggy_project/bloggy_project/settings.py
@@ -129,6 +129,6 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+# )
