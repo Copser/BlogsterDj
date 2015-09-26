8967b8a781bca8a48d0b30424c13803505bf06e7
Copser <iamrmot@gmail.com>
2015-09-26 05:08:39 +0200

    Fix typo

--- bloggy_project/bloggy_project/settings.py
+++ bloggy_project/bloggy_project/settings.py
@@ -126,7 +126,7 @@ except Exception, e:
 import os
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
