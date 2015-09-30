21bdae8f299e3503b62cb98ca0035ad96d842301
Copser <iamrmot@gmail.com>
2015-09-30 06:47:11 +0200

    Updaing fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -2,16 +2,39 @@ from fabric.api import local
 
 
 # Git deployment setup
+def commit():
+    """TODO: Docstring for commit.
+    :returns: TODO
+
+    """
+    local('git add .')
+    print("Enter your git commit message: ")
+    comment = raw_input()
+    local('git commit -m "%s"' % comment)
+
+
+def prep():
+    """TODO: Docstring for prep.
+    :returns: TODO
+
+    """
+    commit()
+
+
+def push():
+    """TODO: Docstring for deploy.
+    :returns: TODO
+
+    """
+    local('git push -u origin master')
+    local('heroku maintenance:on')
+    local('git push heroku master')
+    local('heroku maintenance:off')
+
+
 def deploy():
     """TODO: Docstring for deploy.
     :returns: TODO
 
     """
-    local("git add .")
-    print("Enter your git commit comment: ")
-    comment = raw_input()
-    local('git commit -m "%s"' % comment)
-    local('git push -u origin master')
-    local('heroku maintenance:on')
-    local('git push heroku master')
-    local('heroku maintenance:off')
+    push()
