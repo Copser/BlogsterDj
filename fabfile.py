cd6f216e047d42e8c7119e2c2257cd97e64a8ef1
Copser <iamrmot@gmail.com>
2015-09-30 06:41:29 +0200

    Updating Procfile, fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -2,44 +2,16 @@ from fabric.api import local
 
 
 # Git deployment setup
-def commit():
-    """TODO: Docstring for commit.
-    :returns: TODO
-
-    """
-    message = raw_input("Enter a git commit message: ")
-    local("git add . && git commit -m '{}'".format(message))
-
-
-def push():
-    """TODO: Docstring for push.
-    :returns: TODO
-    push to GitHub
-
-    """
-    local("git push origin master")
-
-
-def prepare():
-    """TODO: Docstring for prepare.
-    :returns: TODO
-
-    """
-    push()
-
-
-# Heroku deployment
-def heroku():
-    """TODO: Docstring for heroku.
-    :returns: TODO
-
-    """
-    local("git push heroku master")
-
-
 def deploy():
     """TODO: Docstring for deploy.
     :returns: TODO
 
     """
-    heroku()
+    local("git add .")
+    print("Enter your git commit comment: ")
+    comment = raw_input()
+    local('git commit -m "%s"' % comment)
+    local('git push -u origin master')
+    local('heroku maintenance:on')
+    local('git push heroku master')
+    local('heroku maintenance:off')
