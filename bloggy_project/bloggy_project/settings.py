d18e827e27e5622aaf2b1161b28b2566d593b22f
Copser <iamrmot@gmail.com>
2015-10-08 22:35:52 +0200

    Setting DEBUG = True

--- bloggy_project/bloggy_project/settings.py
+++ bloggy_project/bloggy_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'oi1+cyk&9g-n*nyiymkjzt6-es@!g7=edzpx+--rdsj4kw&4&3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
